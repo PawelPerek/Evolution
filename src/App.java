ecb38348f116b285fbc260b0eb44c443a274cfaf
Pawel Perek <pawel@perek.pro>
2021-05-04 20:13:25 +0300

    Add resizing

--- src/App.java
+++ src/App.java
@@ -12,8 +12,16 @@ public class App extends Application {
 
     private static Config config;
 
-    private final int WIDTH = 800;
-    private final int HEIGHT = 600;
+    private int width = 1366;
+    private int height = 768;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
     public static void generateConfig() {
         config = new Config(30, 20, 100, 1, 20, 0.3);
@@ -29,22 +37,27 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 800, 600, Color.WHITE);
-        Canvas canvas = new Canvas(800, 600);
+        var root = new Group();
+        var scene = new Scene(root, width, height, Color.WHITE);
+        scene.widthProperty().addListener((obs, oldVal, newVal) -> setWidth(newVal.intValue()));
+        scene.heightProperty().addListener((obs, oldVal, newVal) -> setHeight(newVal.intValue()));
+        var canvas = new Canvas(width, height);
         root.getChildren().add(canvas);
-        GraphicsContext ctx = canvas.getGraphicsContext2D();
+        var ctx = canvas.getGraphicsContext2D();
         stage.setTitle("Evolution");
 
         stage.setScene(scene);
-        stage.setResizable(false);
         stage.show();
 
-        ctx.scale((double) WIDTH / (double) config.width(), (double) HEIGHT / (double) config.height());
+        renderGame(ctx);
+    }
+
+    private void renderMenu(GraphicsContext ctx) {
+        ctx.scale((double) width / (double) config.width(), (double) height / (double) config.height());
         new AnimationTimer() {
             @Override
             public void handle(long currentNanoTime) {
-                
+
                 engine.loop();
                 for (int y = 0; y < config.height(); y++) {
                     for (int x = 0; x < config.width(); x++) {
@@ -57,13 +70,42 @@ public class App extends Application {
                         ctx.strokeRect(x, y, 1, 1);
                     }
                 }
-                
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+    }
+
+    private void renderGame(GraphicsContext ctx) {
+        ctx.scale((double) width / (double) config.width(), (double) height / (double) config.height());
+        new AnimationTimer() {
+            @Override
+            public void handle(long currentNanoTime) {
+
+                engine.loop();
+                for (int y = 0; y < config.height(); y++) {
+                    for (int x = 0; x < config.width(); x++) {
+                        ctx.setStroke(switch (engine.getState(x, y)) {
+                            case Animal -> Color.RED;
+                            case Plant -> Color.YELLOW;
+                            case Jungle -> Color.DARKGREEN;
+                            case Steppe -> Color.GREEN;
+                        });
+                        ctx.strokeRect(x, y, 1, 1);
                     }
-                
+                }
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
             }
         }.start();
     }
